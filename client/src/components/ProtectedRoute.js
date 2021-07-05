042cca504ca644a8c16c92f0deb80c4f436d932c
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2021-07-05 07:35:41 +0530

    Client: Incuding protected route

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+function ProtectedRoute({
+    isAuthenticatedUser: isAuthenticatedUser,
+    component: Component,
+    ...rest
+}) {
+    console.log(`isAuthenticated => `, isAuthenticatedUser);
+    return (
+        <Route
+            {...rest}
+            render={(props) => {
+                if (isAuthenticatedUser) return <Component />;
+                else
+                    return (
+                        <Redirect
+                            to={{
+                                pathname: "/",
+                                state: { from: props.location },
+                            }}
+                        />
+                    );
+            }}
+        />
+    );
+}
+
+export default ProtectedRoute;

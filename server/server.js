7c4a1c1b36f0fce987f7b981259ddf52fe073def
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2022-12-12 22:59:13 +0530

    feat: Add docker support for node

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -12,7 +12,7 @@ const privateRoute = require("./routes/privateRoutes");
 // import middlewares from utilities
 const utilities = require("./util");
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3005;
 
 app.use(cookieParser());
 app.use(express.json());
@@ -27,37 +27,37 @@ app.use(express.json());
 // app.use(cors());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Authorization"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
 
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "POST,GET");
-        return res.status(200).json({});
-    }
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST,GET");
+    return res.status(200).json({ log: "If you are able to see this" });
+  }
 
-    next();
+  next();
 });
 
 app.use("/api/auth", authRoute);
 app.use("/api/private", utilities.verifyToken, privateRoute);
 
-mongoose.connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    // () => console.log(`Database connection established successfully!`)
-    () =>
-        console.log(
-            `mongoose.connection.readyState => ${mongoose.connection.readyState}`
-        )
-);
-mongoose.connection.on("error", (err) => console.error(`Error Occurred!`, err));
+// mongoose.connect(
+//     process.env.MONGO_URI,
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     // () => console.log(`Database connection established successfully!`)
+//     () =>
+//         console.log(
+//             `mongoose.connection.readyState => ${mongoose.connection.readyState}`
+//         )
+// );
+// mongoose.connection.on("error", (err) => console.error(`Error Occurred!`, err));
 
-app.get("/", (_req, res) =>
-    res.send(`<h1>If you're able to see this, then server is started!!</h1>`)
-);
+app.get("/", (_req, res) => {
+  res.send(`<h1>If you're able to see this, then server is started!!</h1>`);
+});
 
 app.listen(PORT, () => console.log(`Server up! PORT: ${PORT}`));
